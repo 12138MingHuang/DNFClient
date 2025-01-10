5c071a1bc444eb67041c1c978be5a104be592b39
张斌 <bin.zhang@appshahe.com>
2025-01-10 18:40:09 +0800

    feat：枪手核弹技能配置

--- Assets/ZMGCFrameWork/BattleWorld/DataMgr/HeroDataMgr.cs
+++ Assets/ZMGCFrameWork/BattleWorld/DataMgr/HeroDataMgr.cs
@@ -22,7 +22,7 @@ namespace ZMGC.Battle
 		private Dictionary<int, int[]> mHeroSkillCfgDic = new Dictionary<int, int[]>
 		{
 			{1000, new int[] { 1004, 1005, 1007, 1008, 1010, 1015 }}, // 英雄1的技能配置
-			{1001, new int[] { 1004, 1005, 3021 }}, // 英雄2的技能配置
+			{1001, new int[] { 1004, 1005, 3021, 3022 }}, // 英雄2的技能配置
 		};
 		
 		public void OnCreate()
